d86174dd84e3d42d5b6bca56c46064bca858e75d
findshen <sx0523@gmail.com>
2019-08-11 22:52:29 +0800

    init

--- common/src/main/java/com/findshen/modules/bz/dao/StudentDao.java
+++ common/src/main/java/com/findshen/modules/bz/dao/StudentDao.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
 
 package com.findshen.modules.bz.dao;
 
@@ -12,11 +5,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.findshen.modules.bz.entity.StudentEntity;
 import org.apache.ibatis.annotations.Mapper;
 
-/**
- * 系统用户
- *
- * @author Mark sunlightcs@gmail.com
- */
 @Mapper
 public interface StudentDao extends BaseMapper<StudentEntity> {
 	
