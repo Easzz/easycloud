3611aa33ac2836f5df92b02dd08b9579011c1b0b
findshen <sx0523@gmail.com>
2021-05-29 17:41:29 +0800

    111

--- fileImport/src/main/resources/project.sql
+++ fileImport/src/main/resources/project.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50730
  File Encoding         : 65001
 
- Date: 28/05/2021 17:08:48
+ Date: 29/05/2021 17:40:58
 */
 
 SET NAMES utf8mb4;
@@ -24,34 +24,22 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `uni_name`(`project_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of project
 -- ----------------------------
+INSERT INTO `project` VALUES (19, '123');
+INSERT INTO `project` VALUES (20, '12312');
+INSERT INTO `project` VALUES (21, 'cesss');
+INSERT INTO `project` VALUES (22, 'sdfsf');
 INSERT INTO `project` VALUES (1, '测试');
-
--- ----------------------------
--- Table structure for project_item
--- ----------------------------
-DROP TABLE IF EXISTS `project_item`;
-CREATE TABLE `project_item`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) NULL DEFAULT NULL COMMENT '项目id',
-  `drive_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '驱动名称',
-  `platform` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '平台',
-  `gmt_create` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of project_item
--- ----------------------------
-INSERT INTO `project_item` VALUES (1, 1, '声卡', 'win10', NULL);
-INSERT INTO `project_item` VALUES (2, 1, '显卡', 'win10', NULL);
-INSERT INTO `project_item` VALUES (3, 1, '声卡', 'win7', NULL);
-INSERT INTO `project_item` VALUES (4, 1, '显卡', 'win7', NULL);
+INSERT INTO `project` VALUES (4, '测试4');
+INSERT INTO `project` VALUES (23, '胜多负少');
+INSERT INTO `project` VALUES (2, '项目2');
+INSERT INTO `project` VALUES (3, '项目3');
 
 -- ----------------------------
 -- Table structure for project_item_sub
@@ -62,18 +50,17 @@ CREATE TABLE `project_item_sub`  (
   `project_id` int(11) NULL DEFAULT NULL COMMENT '项目id',
   `project_item_id` int(11) NULL DEFAULT NULL,
   `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '平台',
-  `gmt_create` datetime(0) NULL DEFAULT NULL,
   `drive_date` datetime(0) NULL DEFAULT NULL,
   `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `drive_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `platform` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `gmt_create` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of project_item_sub
 -- ----------------------------
-INSERT INTO `project_item_sub` VALUES (1, 1, 1, '1.0', NULL, '2021-05-28 14:19:30', 'aa.exe', '声卡', 'win10');
-INSERT INTO `project_item_sub` VALUES (2, 1, 3, '1.1', NULL, '2021-05-06 14:23:05', 'bb.exe', '声卡', 'win7');
+INSERT INTO `project_item_sub` VALUES (23, 22, NULL, '1.0.1', '2021-05-04 04:00:00', '\\upload\\20210529\\3ab1640520fc4aad888432d60d388e58.zip', 'Graphic', 'win7', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
