a996f4ef93b306bce0e95d0a1044366dbd2d9dc9
findshen <sx0523@gmail.com>
2021-05-31 15:56:40 +0800

    111

--- fileImport/src/main/java/com/example/demo/entity/ProjectItemSub.java
+++ fileImport/src/main/java/com/example/demo/entity/ProjectItemSub.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,12 +23,12 @@ public class ProjectItemSub extends Model<ProjectItemSub> {
 	private Long id;
 	@ApiModelProperty(value = "设备id")
 	private Long projectId;
-	private Long projectItemId;
 	private String driveName;
 	private String platform;
 
 	private String version;
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern="yyyy/MM/dd",timezone="GMT+8")
 	private Date driveDate;
 	private String url;
 
