08216ba28436e280ce84361ea71301ee7b2f6239
findshen <sx0523@gmail.com>
2024-05-23 08:54:14 +0800

    case

--- case-project/src/main/java/com/example/caseProject/entity/Item.java
+++ case-project/src/main/java/com/example/caseProject/entity/Item.java
@@ -23,7 +23,7 @@ public class Item extends Model<Item> {
 	private Long id;
 	private String itemName;
 
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	@JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
 	private Date itemDate;
 
 
